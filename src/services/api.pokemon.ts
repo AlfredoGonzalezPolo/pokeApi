9a182466602a4f7a32fb5bd519566c167da64b8c
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-12-14 22:09:40 +0100

    Refactor class ApiPokemon

--- src/services/api.pokemon.ts
+++ src/services/api.pokemon.ts
@@ -2,10 +2,7 @@ import { Pokemon } from '../types/pokemon';
 import { PokemonInfo } from '../types/pokemonInfo';
 
 export class ApiPokemon {
-  pokeUrl: string;
-  serverUrl: string;
-
-  constructor() {
+  constructor(public pokeUrl: string, public serverUrl: string) {
     this.pokeUrl = `https://pokeapi.co/api/v2/pokemon?limit=`;
     this.serverUrl = `http://localhost:3000/pokemon`;
   }
@@ -13,7 +10,7 @@ export class ApiPokemon {
   async getAll(limit: number = 20, offset: number = 0) {
     const response = await fetch(`${this.pokeUrl}${limit}&offset=${offset}`);
     const pokemonList = await response.json();
-    return pokemonList.results;
+    return pokemonList.results as Promise<Pokemon[]>;
   }
 
   async getEach(url: PokemonInfo['url']) {
