38b07e61a5684743e8e0d591cdf30cabe58a85b5
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-12-15 00:24:42 +0100

    Create structural components

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import { Footer } from '../footer/Footer';
+import { Header } from '../header/Header';
+
+export function App() {
+  return (
+    <>
+      <div className="app-container">
+        <Header></Header>
+        <Footer></Footer>
+      </div>
+    </>
+  );
+}
