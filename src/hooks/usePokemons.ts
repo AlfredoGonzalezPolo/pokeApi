51a3b286b354437a080d468b3d11b3a796c7acc7
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-12-19 16:07:41 +0100

    Create usePokemon hook to manage pagination and pokemonList

---
+++ src/hooks/usePokemons.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { Pokemon } from '../types/pokemon';
+import { PokeApiRespository } from '../services/PokeApiRepository.service.ts';
+
+const usePokemons = () => {
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(20);
+  const [pokemons, setPokemons] = useState<Pokemon[] | undefined>([]);
+
+  useEffect(() => {
+    PokeApiRespository.prototype
+      .getPokemonPage(page, limit)
+      .then((pageData) => {
+        setPokemons(pageData?.pokemonsPageData);
+      });
+  }, [page, limit]);
+  return { pokemons, page, setPage, setLimit };
+};
+
+export default usePokemons;
